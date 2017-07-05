1203ef04e04e1e23240122c3fe35eb90b663efa4
buxxi <buxxi@omfilm.net>
2017-07-05 13:41:31 +0200

    Two more subcategories for the drink category.

--- js/questions/drinks.js
+++ js/questions/drinks.js
@@ -2,13 +2,42 @@ function DrinksQuestions($http) {
 	var self = this;
 	var drinks = [];
 	var TOTAL_DRINKS = 50;
+	var types = {
+		all : {
+			title : (correct) => "What drink do you get if you mix the following ingredients?",
+			correct : randomDrink,
+			similar : similarDrinks,
+			load : loadList,
+			format : resolveName,
+			randomAnswer : false,
+			weight : 50
+		},
+		single : {
+			title : (correct) => "Which of these ingredients do you need to make a " + correct.drink.name + "?",
+			correct : randomIngredient,
+			similar : differentIngredients,
+			load : loadBlankIngredient,
+			format : resolveName,
+			randomAnswer : true,
+			weight : 25
+		},
+		glass : {
+			title : (correct) => "What kind of glass should you serve a " + correct.name + " in?",
+			correct : randomDrinkWithGlass,
+			similar : drinksWithGlass,
+			load : loadBlankDrink,
+			format : resolveGlass,
+			randomAnswer : true,
+			weight : 25
+		}
+	};
 
 	self.describe = function() {
 		return {
 			type : 'drinks',
 			name : 'Drinks',
 			icon : 'fa-glass',
-			count : drinks.length
+			count : drinks.length * Object.keys(types).length
 		};
 	}
 
@@ -16,11 +45,16 @@ function DrinksQuestions($http) {
 		return cache.get('drinks', (resolve, reject) => {
 			var result = [];
 
+			function loadRandomDrink() {
+				return $http.get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+			}
+
 			function parseDrink(response) {
 				var data = response.data.drinks[0];
 				var drink = {
 					name : data['strDrink'],
 					ingredients : Object.keys(data).filter((k) => k.indexOf("strIngredient") > -1).map((k) => data[k]).filter((v) => !!v),
+					glass : data['strGlass'],
 					url : 'https://www.thecocktaildb.com/drink.php?c=' + data['idDrink']
 				}
 				result.push(drink);
@@ -49,34 +83,86 @@ function DrinksQuestions($http) {
 
 	self.nextQuestion = function(selector) {
 		return new Promise((resolve, reject) => {
-			var drink = selector.fromArray(drinks);
-
-			function resolveName(d) { return d.name; }
+			var type = selector.fromWeightedObject(types);
+			var correct = type.correct(selector);
 
 			resolve({
-				text : "What drink do you get if you mix the following ingredients?",
-				answers : selector.alternatives(similar(drink, selector), drink, resolveName, selector.first),
-				correct : resolveName(drink),
-				view : {
-					player : 'list',
-					list : drink.ingredients,
-					attribution : {
-						title : "Featured drink",
-						name : drink.name,
-						links : [drink.url]
-					}
-				}
+				text : type.title(correct),
+				answers : selector.alternatives(type.similar(correct, selector), correct, type.format, type.randomAnswer ? selector.splice : selector.first),
+				correct : type.format(correct),
+				view : type.load(correct)
 			});
 		});
 	}
 
-	function similar(drink, selector) {
+	function randomIngredient(selector) {
+		var drink = selector.fromArray(drinks);
+		var ingredient = selector.fromArray(drink.ingredients);
+		return {
+			name : ingredient,
+			drink : drink
+		}
+	}
+
+	function randomDrinkWithGlass(selector) {
+		return selector.fromArray(drinks.filter((d) => d.glass != 'vote'));
+	}
+
+	function randomDrink(selector) {
+		return selector.fromArray(drinks);
+	}
+
+	function drinksWithGlass(drink, selector) {
+		return drinks.filter((d) => d.glass != 'vote');
+	}
+
+	function similarDrinks(drink, selector) {
 		return drinks.slice().sort((a, b) => {
 				return selector.countCommon(drink.ingredients, b.ingredients) - selector.countCommon(drink.ingredients, a.ingredients);
 		});
 	}
 
-	function loadRandomDrink() {
-		return $http.get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+	function differentIngredients(ingredient, selector) {
+		return drinks.filter((d) => selector.countCommon(ingredient.drink.ingredients, d.ingredients) == 0).map((d) => ({name : selector.fromArray(d.ingredients)}));
+	}
+
+	function resolveGlass(correct) {
+		return correct.glass;
+	}
+
+	function resolveName(correct) {
+		return correct.name;
+	}
+
+	function loadBlankDrink(drink) {
+		return {
+			attribution : {
+				title : "Featured drink",
+				name : drink.name,
+				links : [drink.url]
+			}
+		};
+	}
+
+	function loadBlankIngredient(ingredient) {
+		return {
+			attribution : {
+				title : "Featured drink",
+				name : ingredient.drink.name,
+				links : [ingredient.drink.url]
+			}
+		};
+	}
+
+	function loadList(drink) {
+		return {
+			player : 'list',
+			list : drink.ingredients,
+			attribution : {
+				title : "Featured drink",
+				name : drink.name,
+				links : [drink.url]
+			}
+		};
 	}
 }
