af905da8498c145ec975582392018ba511160be2
buxxi <buxxi@omfilm.net>
2018-07-20 11:40:22 +0200

    Using async/await for loading actor data

--- js/questions/actors.js
+++ js/questions/actors.js
@@ -8,7 +8,7 @@ function ActorQuestions($http) {
 		actor_name : {
 			title : (correct) => "Who is this " + (correct.male ? "actor" : "actress") + "?",
 			correct : randomActor,
-			similar : loadSimilarActors,
+			similar : similarActors,
 			format : actorName,
 			view : viewActorImage,
 			count : countActors,
@@ -27,125 +27,120 @@ function ActorQuestions($http) {
 
 	self.preload = function(progress, cache, apikeys) {
         tmdbApiKey = apikeys.tmdb;
-        return new Promise((resolve, reject) => {
-            loadActors(progress, cache).then((data) => {
-                actors = data;
-                resolve();
-            }).catch(reject);
+        return new Promise(async (resolve, reject) => {
+			try {
+				progress(0, ACTOR_COUNT);
+				actors = await loadActors(progress, cache);
+				resolve();
+			} catch (e) {
+				reject(e);
+			}
         });
     }
 
 	self.nextQuestion = function(selector) {
 		return new Promise((resolve, reject) => {
-			var type = selector.fromWeightedObject(types);
-			var attribution = [];
+			let type = selector.fromWeightedObject(types);
+			let attribution = [];
 
-			type.correct(selector, attribution).then((correct) => {
-				type.similar(correct, attribution, selector).then((similar) => {
-					resolve({
-						text : type.title(correct),
-						answers : selector.alternatives(similar, correct, type.format, selector.first),
-						correct : type.format(correct),
-						view : type.view(correct, attribution)
-					});
-				}).catch(reject);
-			}).catch(reject);
+			let actor = type.correct(selector, attribution);
+			let similar = type.similar(actor, attribution, selector);
+
+			resolve({
+				text : type.title(actor),
+				answers : selector.alternatives(similar, actor, type.format, selector.first),
+				correct : type.format(actor),
+				view : type.view(actor, attribution)
+			});
 		});
 	}
 
 	function loadActors(progress, cache) {
-		return cache.get('actors', (resolve, reject) => {
-			var result = [];
+		return cache.get('actors', async (resolve, reject) => {
+			let result = [];
+			let page = 1;
 
-			function loadActorDetails(index) {
-				if (!index) {
-					index = 0;
+			try {
+				while (result.length < ACTOR_COUNT) {
+					let actorsChunk = await loadActorsChunk(page);
+					for (actor of actorsChunk) {
+						actor = await loadActorDetails(actor);
+						result.push(actor);
+						progress(result.length, ACTOR_COUNT);
+					}
 				}
-
-				return new Promise((detailResolve, detailReject) => {
-					var actor = result[index];
-					$http.get('https://api.themoviedb.org/3/person/' + actor.id, {
-						params : {
-							api_key : tmdbApiKey
-						}
-					}).then((response) => {
-						progress(index, ACTOR_COUNT);
-
-						var obj = response.data;
-						Object.assign(actor, {
-							name : obj.name,
-							photo : obj.profile_path,
-							birthday : new Date(Date.parse(obj.birthday)),
-							male : obj.gender == 2
-						});
-
-						index++;
-						if (index == result.length) {
-							detailResolve();
-						} else {
-							loadActorDetails(index).then(detailResolve).catch(detailReject);
-						}
-					}).catch((err) => {
-						if (err.status == 429) {
-							var time = (parseInt(err.headers()['retry-after']) + 1) * 1000;
-							setTimeout(() => {
-								loadActorDetails(index).then(detailResolve).catch(detailReject);
-							}, time);
-							return;
-						}
-						detailReject(err);
-					});
-				});
-			};
-
-			function loadPage(page) {
-				return new Promise((pageResolve, pageReject) => {
-					$http.get('https://api.themoviedb.org/3/person/popular', {
-						params : {
-							api_key : tmdbApiKey,
-							page : page
-						}
-					}).then((response) => {
-						result = result.concat(response.data.results.filter((actor) => !actor.adult).map((actor) => {
-							return {
-								id : actor.id
-							};
-						}));
-						if (result.length < ACTOR_COUNT) {
-							loadPage(page++).then(pageResolve).catch(pageReject);
-						} else {
-							pageResolve();
-						}
-					}).catch(pageReject);
-				});
+			} catch (e) {
+				reject(e);
 			}
-
-			loadPage(1).then(loadActorDetails).then(() => { resolve(result) }).catch(reject);
+			resolve(result);
 		});
 	}
 
-	function loadSimilarActors(actor, attribution, selector) {
+	function loadActorsChunk(page) {
 		return new Promise((resolve, reject) => {
-			function sameGender(a, b) {
-				return a.male == b.male;
-			}
-			function aboutSameAge(a, b) {
-				if (!a.birthday || !b.birthday) {
-					return true;
+			$http.get('https://api.themoviedb.org/3/person/popular', {
+				params : {
+					api_key : tmdbApiKey,
+					page : page
 				}
-				return Math.abs(a.birthday.getFullYear() - b.birthday.getFullYear()) <= 5;
-			}
-
-			resolve(actors.filter((a) => sameGender(a, actor) && aboutSameAge(a - actor)));
+			}).then((response) => {
+				let result = response.data.results.filter((actor) => !actor.adult).map((actor) => {
+					return {
+						id : actor.id
+					};
+				});
+				resolve(result);
+			}).catch(reject);
 		});
+	}
+
+	function loadActorDetails(actor) {
+		return new Promise((resolve, reject) => {
+			$http.get('https://api.themoviedb.org/3/person/' + actor.id, {
+				params : {
+					api_key : tmdbApiKey
+				}
+			}).then((response) => {
+				let obj = response.data;
+				Object.assign(actor, {
+					name : obj.name,
+					photo : obj.profile_path,
+					birthday : new Date(Date.parse(obj.birthday)),
+					male : obj.gender == 2
+				});
+
+				resolve(actor);
+			}).catch((err) => {
+				if (err.status == 429) {
+					let time = (parseInt(err.headers()['retry-after']) + 1) * 1000;
+					setTimeout(() => {
+						loadActorDetails(actor).then(resolve).catch(reject);
+					}, time);
+					return;
+				}
+				reject(err);
+			});
+		});
+	};
+
+	function similarActors(actor, attribution, selector) {
+		function sameGender(a, b) {
+			return a.male == b.male;
+		}
+		function aboutSameAge(a, b) {
+			if (!a.birthday || !b.birthday) {
+				return true;
+			}
+			return Math.abs(a.birthday.getFullYear() - b.birthday.getFullYear()) <= 5;
+		}
+
+		return actors.filter((a) => sameGender(a, actor) && aboutSameAge(a - actor));
 	}
 
 	function randomActor(selector, attribution) {
-		return new Promise((resolve, reject) => {
-			var actor = selector.fromArray(actors);
-			attribution.push("http://www.themoviedb.org/person/" + actor.id);
-			resolve(actor);
-		});
+		var actor = selector.fromArray(actors);
+		attribution.push("http://www.themoviedb.org/person/" + actor.id);
+		return actor;
 	}
 
 	function actorName(actor) {
