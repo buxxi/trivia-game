fd80da24279e973e22fb2f1af1e7a926301a369b
buxxi <buxxi@omfilm.net>
2021-05-24 21:06:55 +0200

    using node-sass instead of middleware

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const path = require('path');
-const sass = require('node-sass-middleware');
+const sass = require('node-sass');
 const ws = require('ws');
 const app = express();
 const port = 5555;
 
-app.use('/index.html', express.static('index.html'));
+let clientStyle = sass.renderSync({
+	file: 'client/css/client.scss'
+}).css.toString();
+let monitorStyle = sass.renderSync({
+	file: 'monitor/css/monitor.scss'
+}).css.toString();
 
-app.use(sass({
-	src: path.join(__dirname, '..'),
-	dest: path.join(__dirname, '..')
-}));
+app.use('/index.html', express.static('index.html'));
 
 //Serve files for client
 app.use('/client', express.static('client'));
+app.get('/client/css/client.css', (req, res) => { res.type("css").send(clientStyle); });
 app.use('/client/css', express.static('css'));
 app.use('/client/js', express.static('js'));
-app.use('/client/js/avatars.js', express.static('js/avatars.js'));
-app.use('/client/js/connection.js', express.static('js/connection.js'));
 app.use('/client/fonts', express.static('fonts'));
 app.use('/client/img/simple_dashed.png', express.static('img/simple_dashed.png'));
 
 //Serve files for monitor
 app.use('/monitor', express.static('monitor'));
+app.get('/monitor/css/monitor.css', (req, res) => { res.type("css").send(monitorStyle); });
 app.use('/monitor/css', express.static('css'));
 app.use('/monitor/js', express.static('js'));
-app.use('/monitor/js/avatars.js', express.static('js/avatars.js'));
-app.use('/monitor/js/connection.js', express.static('js/connection.js'));
 app.use('/monitor/fonts', express.static('fonts'));
 app.use('/monitor/img/simple_dashed.png', express.static('img/simple_dashed.png'));
 
