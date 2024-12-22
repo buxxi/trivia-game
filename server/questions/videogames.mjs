e9d638556fa2cda002497d0e159751a183d5d03c
buxxi <buxxi@omfilm.net>
2024-12-22 15:47:22 +0100

    Filter out remakes and remasters when loading video games

--- server/questions/videogames.mjs
+++ server/questions/videogames.mjs
@@ -19,36 +19,36 @@ class VideoGameQuestions extends Questions {
 		this._igdbBaseURL = 'https://api.igdb.com/v4/';
 
 		this._addQuestion({
-			title : () => "Which game is this a screenshot of?",
-			correct : () => this._randomGame(),
-			similar : (correct) => this._similarGames(correct),
-			load : (correct) => this._screenshot(correct),
-			format : (correct) => this._gameTitle(correct),
-			weight : 45
+			title: () => "Which game is this a screenshot of?",
+			correct: () => this._randomGame(),
+			similar: (correct) => this._similarGames(correct),
+			load: (correct) => this._screenshot(correct),
+			format: (correct) => this._gameTitle(correct),
+			weight: 45
 		});
 		this._addQuestion({
-			title : (correct) => "In which year was '" + correct.name + "' first released?",
-			correct : () => this._randomGame(),
-			similar : (correct) => this._similarGameYears(correct),
-			load : (correct) => this._blank(correct),
-			format : (correct) => this._gameYear(correct),
-			weight : 10
+			title: (correct) => "In which year was '" + correct.name + "' first released?",
+			correct: () => this._randomGame(),
+			similar: (correct) => this._similarGameYears(correct),
+			load: (correct) => this._blank(correct),
+			format: (correct) => this._gameYear(correct),
+			weight: 10
 		});
 		this._addQuestion({
-			title : (correct) => "'" + correct.name + "' was released to one of these platforms, which one?",
-			correct : () => this._randomGame(),
-			similar : (correct) => this._similarPlatforms(correct),
-			load : (correct) => this._blank(correct),
-			format : (correct) => this._gamePlatform(correct),
-			weight : 10
+			title: (correct) => "'" + correct.name + "' was released to one of these platforms, which one?",
+			correct: () => this._randomGame(),
+			similar: (correct) => this._similarPlatforms(correct),
+			load: (correct) => this._blank(correct),
+			format: (correct) => this._gamePlatform(correct),
+			weight: 10
 		});
 	}
 
 	describe() {
 		return {
-			name : 'Video Games',
-			icon : 'fa-gamepad',
-			attribution : [
+			name: 'Video Games',
+			icon: 'fa-gamepad',
+			attribution: [
 				{ url: 'https://youtube.com', name: 'YouTube' },
 				{ url: 'https://www.igdb.com', name: 'IGDB' }
 			]
@@ -58,7 +58,7 @@ class VideoGameQuestions extends Questions {
 	async preload(progress, cache) {
 		let token = await this._loadTwitchAccessToken();
 		this._platforms = await this._loadPlatforms(cache, token);
-		
+
 		let toLoadPlatforms = Object.keys(this._platforms);
 		let total = toLoadPlatforms.length * GAMES_PER_PLATFORM;
 
@@ -73,7 +73,7 @@ class VideoGameQuestions extends Questions {
 		}
 
 		this._games = games;
-		
+
 		return this._countQuestions();
 	}
 
@@ -93,14 +93,14 @@ class VideoGameQuestions extends Questions {
 	_loadGames(platform, platforms, cache, token) {
 		return cache.get(platform, async (resolve, reject) => {
 			try {
-				let inputData = `fields name,url,first_release_date,platforms,screenshots,keywords,themes,genres; where platforms = (${platform}) & first_release_date != null & screenshots != null & rating_count > 2; limit ${GAMES_PER_PLATFORM}; offset 0; sort rating desc;`;
-				let response = await fetch(this._igdbBaseURL + 'games/',{
-					method : 'POST',
-					headers : {
-						'Client-ID' : this._igdbClientId,
+				let inputData = `fields name,url,first_release_date,platforms,screenshots,keywords,themes,genres; where platforms = (${platform}) & first_release_date != null & screenshots != null & rating_count > 2 & category = 0 & version_parent = null; limit ${GAMES_PER_PLATFORM}; offset 0; sort rating desc;`;
+				let response = await fetch(this._igdbBaseURL + 'games/', {
+					method: 'POST',
+					headers: {
+						'Client-ID': this._igdbClientId,
 						'Authorization': `Bearer ${token}`
 					},
-					body : inputData
+					body: inputData
 				});
 				let data = await this._toJSON(response);
 
@@ -116,17 +116,17 @@ class VideoGameQuestions extends Questions {
 
 				let games = data.map((game) => {
 					return {
-						name : game.name,
-						release_date : release_date(game.first_release_date),
-						screenshots : game.screenshots.slice(0,3),
-						platforms : game.platforms.map((p) => platforms[p] ? platforms[p].name : null).filter((p, i, arr) => p != null && arr.indexOf(p) == i),
-						tags : [].concat(tag('k', game.keywords)).concat(tag('t', game.themes)).concat(tag('g', game.genres)),
-						attribution : game.url
+						name: game.name,
+						release_date: release_date(game.first_release_date),
+						screenshots: game.screenshots.slice(0, 3),
+						platforms: game.platforms.map((p) => platforms[p] ? platforms[p].name : null).filter((p, i, arr) => p != null && arr.indexOf(p) == i),
+						tags: [].concat(tag('k', game.keywords)).concat(tag('t', game.themes)).concat(tag('g', game.genres)),
+						attribution: game.url
 					};
 				});
 				let result = await this._loadScreenshots(games, token);
 				resolve(result);
-			} catch(e) {
+			} catch (e) {
 				reject(e);
 			};
 		});
@@ -148,12 +148,12 @@ class VideoGameQuestions extends Questions {
 	async _loadScreenshotChunk(ids, token) {
 		let inputData = `fields id,image_id; where id = (${ids.join(',')}); limit ${PAGINATE_COUNT};`;
 		let response = await fetch(this._igdbBaseURL + 'screenshots/', {
-			method : 'POST',
-			headers : {
-				'Client-ID' : this._igdbClientId,
+			method: 'POST',
+			headers: {
+				'Client-ID': this._igdbClientId,
 				'Authorization': `Bearer ${token}`
 			},
-			body : inputData
+			body: inputData
 		});
 		let data = await this._toJSON(response);
 		let result = {};
@@ -173,7 +173,7 @@ class VideoGameQuestions extends Questions {
 					result = Object.assign(result, chunkResult);
 				} while (false && Object.keys(chunkResult).length == PAGINATE_COUNT);
 				resolve(result);
-			} catch(e) {
+			} catch (e) {
 				reject(e);
 			}
 		});
@@ -182,18 +182,18 @@ class VideoGameQuestions extends Questions {
 	async _loadPlatformChunk(offset, token) {
 		let inputData = `fields id,name; where category = (1,5); limit ${PAGINATE_COUNT}; offset ${offset};`
 		let response = await fetch(this._igdbBaseURL + 'platforms/', {
-			method : 'POST',
-			headers : {
-				'Client-ID' : this._igdbClientId,
+			method: 'POST',
+			headers: {
+				'Client-ID': this._igdbClientId,
 				'Authorization': `Bearer ${token}`
 			},
-			body : inputData
+			body: inputData
 		});
 		let data = await this._toJSON(response);
 		let result = {};
 		data.forEach((platform) => {
 			result[platform.id] = {
-				name : platform.name
+				name: platform.name
 			}
 		});
 		return result;
@@ -207,15 +207,15 @@ class VideoGameQuestions extends Questions {
 		var titleWords = Selector.wordsFromString(game.name);
 		let result = this._games.map((g) => {
 			return {
-				game : g,
-				score : Selector.levenshteinDistance(titleWords, Selector.wordsFromString(g.name)) + Selector.levenshteinDistance(game, g, e => e.tags) + Selector.dateDistance(game.release_date, g.release_date)
+				game: g,
+				score: Selector.levenshteinDistance(titleWords, Selector.wordsFromString(g.name)) + Selector.levenshteinDistance(game, g, e => e.tags) + Selector.dateDistance(game.release_date, g.release_date)
 			};
 		}).sort((a, b) => a.score - b.score).map((node) => node.game);
 		return Generators.inOrder(result);
 	}
 
 	_similarGameYears(game) {
-		return Generators.years(this._gameYear(game), (year) => ({ release_date : year }));
+		return Generators.years(this._gameYear(game), (year) => ({ release_date: year }));
 	}
 
 	_similarPlatforms(game) {
@@ -225,28 +225,28 @@ class VideoGameQuestions extends Questions {
 
 		let unused = Object.keys(this._platforms).map((p) => this._platforms[p]).filter((p) => game.platforms.indexOf(p) == -1).sort(dateDifference);
 
-		let result = unused.map(platform => ({ 'platforms' : [platform.name] }));
+		let result = unused.map(platform => ({ 'platforms': [platform.name] }));
 		return Generators.inOrder(result);
 	}
 
 	_screenshot(game) {
 		return {
-			player : 'image',
-			url : 'https://images.igdb.com/igdb/image/upload/t_screenshot_huge/' + Random.fromArray(game.screenshots) + '.jpg',
-			attribution : {
-				title : "Screenshot of",
-				name : this._gameTitle(game) + " (" + this._gameYear(game) + ")",
-				links : [game.attribution]
+			player: 'image',
+			url: 'https://images.igdb.com/igdb/image/upload/t_screenshot_huge/' + Random.fromArray(game.screenshots) + '.jpg',
+			attribution: {
+				title: "Screenshot of",
+				name: this._gameTitle(game) + " (" + this._gameYear(game) + ")",
+				links: [game.attribution]
 			}
 		}
 	}
 
 	_blank(game) {
 		return {
-			attribution : {
-				title : "Game",
-				name : this._gameTitle(game) + " (" + this._gameYear(game) + ")",
-				links : [game.attribution]
+			attribution: {
+				title: "Game",
+				name: this._gameTitle(game) + " (" + this._gameYear(game) + ")",
+				links: [game.attribution]
 			}
 		}
 	}
